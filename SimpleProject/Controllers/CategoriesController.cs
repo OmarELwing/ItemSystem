9e631f0e4082c2746d8c977b7d24052c2c6ee5f5
basmalamahmoud5 <basmalamahmoud548@gmail.com>
2026-09-02 23:26:38 +0300

    CreateCategoryCotroller

---
+++ SimpleProject/Controllers/CategoriesController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleProject.Data.Models;
+using SimpleProject.Data.UnitOfWork;
+using SimpleProject.DTOs;
+
+namespace SimpleProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoriesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _unitOfWork.Categories.GetAllAsync();
+
+            var result = categories.Select(c => new CategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name
+            }).ToList();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _unitOfWork.Categories.GetByIdAsync(id);
+
+            if (category == null)
+                return NotFound();
+
+            var result = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryDto dto)
+        {
+            var category = new Category
+            {
+                Name = dto.Name
+            };
+
+            await _unitOfWork.Categories.AddAsync(category);
+            await _unitOfWork.SaveAsync();
+
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, CreateCategoryDto dto)
+        {
+            var category = await _unitOfWork.Categories.GetByIdAsync(id);
+
+            if (category == null)
+                return NotFound();
+
+            category.Name = dto.Name;
+
+            _unitOfWork.Categories.Update(category);
+            await _unitOfWork.SaveAsync();
+
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _unitOfWork.Categories.GetByIdAsync(id);
+
+            if (category == null)
+                return NotFound();
+
+            _unitOfWork.Categories.Delete(category);
+            await _unitOfWork.SaveAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ SimpleProject/DTOs/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleProject.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
